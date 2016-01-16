55aabc8592048301b4383ce595574855166ff64f
kenta.nakai <kenta.nakai@urouro.net>
2016-01-16 21:48:17 +0900

    Replace top page UI to Storyboard

--- IdeaCards/AppDelegate.m
+++ IdeaCards/AppDelegate.m
@@ -7,38 +7,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
     NSDictionary *env = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Env" ofType:@"plist"]];
     [[DeployGateSDK sharedInstance] launchApplicationWithAuthor:env[@"DeployGate"][@"Author"]
                                                             key:env[@"DeployGate"][@"APIKey"]];
     
-    JASidePanelController *baseController = [[JASidePanelController alloc] init];
-    TopController *topController = [[TopController alloc] init];
-    EditController *leftController = [[EditController alloc] init];
-    leftController.type = EditTypeLeft;
-    EditController *rightController = [[EditController alloc] init];
-    rightController.type = EditTypeRight;
-    
-    UINavigationController *topNav = [[UINavigationController alloc] initWithRootViewController:topController];
-    [topNav setNavigationBarHidden:YES];
-    UINavigationController *leftNav = [[UINavigationController alloc] initWithRootViewController:leftController];
-    [leftNav setNavigationBarHidden:NO];
-    UINavigationController *rightNav = [[UINavigationController alloc] initWithRootViewController:rightController];
-    [rightNav setNavigationBarHidden:NO];
-    
-    baseController.leftPanel = leftNav;
-    baseController.centerPanel = topNav;
-    baseController.rightPanel = rightNav;
-    
-    baseController.leftGapPercentage = 0.9f;
-    baseController.rightGapPercentage = 0.9f;
-    
-    self.window.rootViewController = baseController;
-    self.basePanelController = baseController;
-    
-    self.window.backgroundColor = [UIColor blackColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
