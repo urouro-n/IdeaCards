1a5c8cd6c106ce772836131566ae1d5d9a7c9cc1
kenta.nakai <kenta.nakai@urouro.net>
2016-02-20 20:20:47 +0900

    Fix import library

--- IdeaCards/BridgingHeader.h
+++ IdeaCards/BridgingHeader.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "JASidePanelController.h"
+#import <JASidePanels/JASidePanelController.h>
 #import <iCarousel/iCarousel.h>
 
 #import "EditController.h"
--- IdeaCards/Classes/Controller/PanelBaseController.swift
+++ IdeaCards/Classes/Controller/PanelBaseController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import JASidePanels
 
 class PanelBaseController: JASidePanelController {
     
