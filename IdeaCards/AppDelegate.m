22fb058929f54b0f8241bafc1ae32c09aff5e2d5
kenta.nakai <kenta.nakai@urouro.net>
2017-03-03 19:54:39 +0900

    Deintegrate DeployGate

--- IdeaCards/AppDelegate.m
+++ IdeaCards/AppDelegate.m
@@ -1,21 +1,11 @@
 #import "AppDelegate.h"
 #import "TopController.h"
-#import <DeployGateSDK/DeployGateSDK.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSDictionary *env = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Env" ofType:@"plist"]];
-    [[DeployGateSDK sharedInstance] launchApplicationWithAuthor:env[@"DeployGate"][@"Author"]
-                                                            key:env[@"DeployGate"][@"APIKey"]];
-    
     return YES;
-}
-
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(nonnull id)annotation
-{
-    return [[DeployGateSDK sharedInstance] handleOpenUrl:url sourceApplication:sourceApplication annotation:annotation];
 }
 
 @end
